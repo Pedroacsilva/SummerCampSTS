4dbda126ef0c47310e276a545a38fba0be7be7cf
DuBento <duarte.bento@tecnico.ulisboa.pt>
2022-08-09 11:10:41 +0100

    Lambda update for json parse

--- lambdaAPI.js
+++ lambdaAPI.js
@@ -3,13 +3,12 @@ var iotdata = new AWS.IotData({ endpoint: 'a1yj9lgdqxsds2-ats.iot.eu-west-2.amaz
 
 exports.handler = async (event) => {
     console.log("Event => " + JSON.stringify(event));
+
+    let led_state = JSON.parse(event["body"])["led"]
     
-    // await iotdata.getThingShadow({thingName: 'grupo3'}, function(err, data) {
-    //   if (err) console.log(err, err.stack); // an error occurred
-    //   else     console.log(data);           // successful response
-    // });
-    
-    let led_state = + event["led"]
+    led_state = led_state ? 1 : 0
+
+    console.log(led_state)
     
     var params = {
         topic: "$aws/things/grupo3/shadow/update",
